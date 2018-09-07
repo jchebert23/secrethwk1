ff268bff5034bdf2ff01f5f02360acc828ef5574
Hebert Clayton <jch223@zoo.cs.yale.edu>
2018-09-07 12:58:43 -0400

    r is basically done

--- farthing.c
+++ farthing.c
@@ -8,6 +8,9 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <linux/limits.h>
+//IMPORTANT AM I ALLOWED TO INCLUDE BELOW LINE
+#include <inttypes.h>
+
 
 int debugPrint1 = 0;
 int debugPrint2 = 1;
@@ -51,6 +54,24 @@ void addToList(struct linkedList *l, char *str)
 	l->tail=e;
 }
 
+int searchList(struct linkedList *l, char *string)
+{
+  struct elt *e;
+  e = l-> head;
+  while(e != 0)
+  {
+    if(strcmp(string, e->str)==0)
+    {
+      return 1;
+    }
+    else
+    {
+      e = e->next;
+    }
+  }
+  return 0;
+}
+
 void linkedListDestroy(struct linkedList *l, int t)
 {
 	struct elt *e;
@@ -72,24 +93,34 @@ void replace(struct linkedList *l, char *archiveFileName){
   
   FILE *f = fopen(archiveFileName, "r");
   char c;
- int  endOfFile = 0;
+  int  endOfFile = 0;
+  char template[]= "tempArchiveXXXXXX";
+  int tempFile = mkstemp(template);
+  FILE *tempFile2 = fdopen(tempFile, "w");
   while(!feof(f))
   {
-  
- 
+    int fileNameSizeIndex=0;
+    char *fileNameSize = malloc(sizeof(char) *4);
     while((c = fgetc(f)) != '|')
     {
+      //if we reach end of file, got out of this
       if(c==EOF)
       {
         endOfFile= 1;
         break;
       }
+      fileNameSize[fileNameSizeIndex] = c;
+      fileNameSizeIndex++;
     }
     if(endOfFile)
     {
       break;
     }
   
+    if(debugPrint2)
+    {
+      printf("file name size: %s\n", fileNameSize);
+    }
     char *absolutePath = malloc(sizeof(char) * PATH_MAX);
     int pathNameIndex=0;
     while((c = fgetc(f)) != '\n')
@@ -97,22 +128,72 @@ void replace(struct linkedList *l, char *archiveFileName){
       absolutePath[pathNameIndex] = c;
       pathNameIndex++;
     }
-    
-    
-    
-    char *fileContents = malloc(sizeof(char) *8192);
-    int contentIndex = 0;
-    while((c = fgetc(f) != '|'))
-    {}
+     //IMPORTANT: MIGHT HAVE TO SWITCH THESE CHARS TO INTS
+    int fileSizeIndex = 0;
+    char *fileSize = malloc(sizeof(char) *4);
+    while((c = fgetc(f)) != '|')
+     {
+       fileSize[fileSizeIndex] = c;
+       fileSizeIndex++;
+     }
 
+    if(debugPrint2)
+    {
+      printf("file size: %s\n", fileSize);
+    }
+
+    char *fileContents = malloc(sizeof(char) *atoi(fileSize));
+    int contentIndex = 0;
+    
     while((c = fgetc(f)) != '\n')
     {
       fileContents[contentIndex] = c;
       contentIndex++;
     }
-    
+    //if the absolute path appears in the arguments
+    if(searchList(l, absolutePath))
+    {
+      struct stat buff;
+      if(lstat(absolutePath, &buff) == -1)
+      {
+        printf("File %s Does Not Exist", absolutePath);
+        exit(0);
+      }
+      else
+      {
+        fprintf(tempFile2, "%ld", strlen(absolutePath));
+      fputs("|", tempFile2);
+      fputs(absolutePath, tempFile2);
+      fputs("\n", tempFile2);
+      //GOT THIS LINE FROM ANDREW SHEINBERG, properly formats the size returned by the lstat function to write it to the file
+      fprintf(tempFile2, "%jd", (intmax_t)(buff.st_size));
+      //GOT ABOVE LINE FROM ANDREW SHEINBERG
+      fputs("|", tempFile2);
+      FILE *NEWCONTENTS = fopen(absolutePath , "r");
+      int fileChar;
+      while((fileChar = fgetc(NEWCONTENTS)) != EOF)
+      {
+        fputc(fileChar, tempFile2);
+      }
+      fclose(NEWCONTENTS);
+      }
+      
+    }
+    else
+    {
+      fprintf(tempFile2, "%ld" , strlen(absolutePath));
+      fputs("|", tempFile2);
+      fputs(absolutePath, tempFile2);
+      fputs("\n", tempFile2);
+      fputs(fileSize, tempFile2);
+      fputs("|", tempFile2);
+      fputs(fileContents, tempFile2); 
+      fputs("\n", tempFile2);
+    }
 
   }
+  fclose(tempFile2);
+  fclose(f);
 }
 
 
@@ -146,22 +227,12 @@ int main(int argc, char**argv)
 
 linkedList *names = linkedListCreate();
 
-for(int i=0; i<=argc; i++)
+for(int i=0; i<argc; i++)
 { printf("Arg %d %s\n", i, argv[i]);
 }
 
-for(int i=3; i<=argc; i++){
+for(int i=3; i<argc; i++){
     addToList(names, argv[i]);
-}
-
-  if(debugPrint2)
-  {
-    printf("Line %d\n", __LINE__);
-  }
-
-if(debugPrint1)
-{
-  printf("%s, %s, %s", names->head->str, names->head->next->str, names->head->next->next->str);
 }
 
 
